0834cb411668c3493416012dcbe9d7aef43df9c3
Taziev Rodion <morrowcj81@gmail.com>
2018-05-06 15:09:54 +0700

    Landing Conference adaptive v totally completed

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    /* Block navigation*/
+
+    $(".for_scroll").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 900);
+    });
+
+});
